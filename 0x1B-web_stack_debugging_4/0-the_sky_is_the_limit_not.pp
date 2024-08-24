35366eb2386b2cd0f829a68ab6cba7c04a6f44f9
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-08-24 10:47:24 +0000

    web stack refactored

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# sets file limite for nginx to 2000 (default is 15)
 exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
